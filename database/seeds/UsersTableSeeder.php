d72e80e2b4a494829f3e1330929057b09b80a174
Monty2107 <monterrosadelgado@gmail.com>
2018-05-28 19:59:06 -0600

    Listo

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,19 +14,19 @@ class UsersTableSeeder extends Seeder
         DB::table('users')->insert([
             'name' => 'Financiera Nombre',
             'email' => 'financiero@gmail.com',
-            'password' => bcrypt('finan0721'),//ih4DdtLkq6
+            'password' => bcrypt('ih4DdtLkq6'),//ih4DdtLkq6
             'cargo'=>'Financiero'
         ]);
         DB::table('users')->insert([
             'name' => 'Administracion Nombre',
             'email' => 'administrador@gmail.com',
-            'password' => bcrypt('admin2107'),//JkBR5dkTLQ
+            'password' => bcrypt('JkBR5dkTLQ'),//JkBR5dkTLQ
             'cargo'=>'Administracion'
         ]);
         DB::table('users')->insert([
             'name' => 'Benjamin Monterrosa',
             'email' => 'programador@gmail.com',
-            'password' => bcrypt('root12345'),//lW0SmtOGDs,str_random(10)
+            'password' => bcrypt('lW0SmtOGDs'),//lW0SmtOGDs,str_random(10)
             'cargo'=>'Programador'
         ]);
     }
